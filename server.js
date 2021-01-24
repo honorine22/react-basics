fdf877d33e2a9c841823570cc054d5713c55b399
honorine22 <higiranez@gmail.com>
2021-01-24 16:46:11 +0200

    start of contact keeper project

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
-app.get(',', (req, res) => 
+// Connect Database
+connectDB();
+
+//Init middleware
+app.use(express.json({ extended: true }));
+
+app.get('/', (req, res) => 
     res.json({ msg: 'Welcome to the ContactKeeper API...'})
 );
 
